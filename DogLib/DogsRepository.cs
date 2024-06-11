c5bc3cb3e72b0c8a8a6aaceda7a81db0f190d7fa
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-06-11 23:34:02 +0200

    Add project files.

---
+++ DogLib/DogsRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogLib
+{
+    public class DogsRepository
+    {
+        private List<Dog> dogs = new List<Dog>();
+        private int nextId = 4;
+
+        public DogsRepository()
+        {
+            Add(new Dog { Id = 1, Name = "Fido", Age = 3 });
+            Add(new Dog { Id = 2, Name = "Rex", Age = 5 });
+            Add(new Dog { Id = 3, Name = "Spot", Age = 2 });
+        }
+        public List<Dog> GetAll(string? name = null, int age = 0)
+        {
+            var result = dogs.AsEnumerable();
+            if(name != null)
+            {
+                result = dogs.Where(d => d.Name == name);
+            }
+            else if(age > 0)
+            {
+                result = dogs.Where(d => d.Age == age);
+            }
+            return result.ToList();
+        }
+        public Dog? GetById(int id)
+        {
+            Dog? dog = dogs.FirstOrDefault(d => d.Id == id);
+            if (dog == null)
+            {
+                throw new ArgumentException("No dog found with the specified Id.");
+            }
+            return dog;
+        }
+        public Dog Add(Dog dog)
+        {
+            dog.Id = nextId++;
+            dogs.Add(dog);
+            return dog;
+        }
+        public Dog? Delete(int id)
+        {
+            Dog? dog = dogs.FirstOrDefault(d => d.Id == id);
+            if (dog == null)
+            {
+                throw new ArgumentException("No dog found with the specified Id.");
+            }
+            dogs.Remove(dog);
+            return dog;
+        }
+
+    }
+}
---
+++ DogLibTests/DogsRepositoryTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DogLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogLib.Tests
+{
+    [TestClass()]
+    public class DogsRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //Arrange
+            int expectedCount = 3;
+            DogsRepository dogsRepository = new DogsRepository();
+            //Act
+            IEnumerable<Dog> dogs = dogsRepository.GetAll();
+            //Assert
+            Assert.AreEqual(expectedCount, dogs.Count());
+        }
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            //Arrange
+            int IdToFind = 2;
+            DogsRepository dogsRepository = new DogsRepository();
+            //Act
+            Dog? dog = dogsRepository.GetById(IdToFind);
+            //Assert
+            Assert.AreEqual(IdToFind, dog?.Id);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            //Arrange
+            DogsRepository dogsRepository = new DogsRepository();
+            Dog dog = new Dog { Name = "Rover", Age = 4 };
+            //Act
+            Dog? addedDog = dogsRepository.Add(dog);
+            //Assert
+            Assert.AreEqual(dog, addedDog);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            //Arrange
+            DogsRepository dogsRepository = new DogsRepository();
+            int IdToDelete = 2;
+            //Act
+            Dog? deletedDog = dogsRepository.Delete(IdToDelete);
+            //Assert
+            Assert.AreEqual(IdToDelete, deletedDog?.Id);
+        }
+    }
+}
